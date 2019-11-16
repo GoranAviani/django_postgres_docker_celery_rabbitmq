e47563c664e4f1136f1eb9f1dbc2d6c88d02326e
goranaviani <goranaviani@gmail.com>
2019-11-16 16:48:50 +0100

    celery file

---
+++ django_postgres_docker_celery_rabbitmq/celery.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_docker_celery_rabbitmq.settings')
+
+app = Celery('django_postgres_docker_celery_rabbitmq',
+             broker='amqp://rabbitmq',
+             backend='amqp://rabbitmq',
+             )
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
