d02659e5eb05e036768c6502d5d945666d9729a2
goranaviani <goranaviani@gmail.com>
2019-11-16 16:50:57 +0100

    quickfix

--- django_postgres_docker_celery_rabbitmq/celery.py
+++ django_postgres_docker_celery_rabbitmq/celery.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_docker_celery_rabbitmq.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_postgres_docker_celery_rabbitmq.settings')
 
 app = Celery('django_postgres_docker_celery_rabbitmq',
              broker='amqp://rabbitmq',
